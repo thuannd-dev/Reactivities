6c8777ad5629ad9ca490d9b554e328641f0159bc
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-08-18 15:57:31 +0700

    walking skeleton part 2 - client

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,18 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 //*********************** Configure the HTTP request pipeline.
 
+/*
+Adds a CORS middleware to your web application pipeline to allow cross domain requests.
+*/
+app.UseCors(options => options.AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+                                
 /*
 * MapControllers middleware provide the routing for application.
 * It maps - pass the incoming HTTP requests to the appropriate- phù hợp controller actions.
