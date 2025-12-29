bd5417e6540c5602b7bce88dcb61e0bf23d5fd71
thuannd-dev <nguyenduongthuan75321@gmail.com>
2025-12-29 16:59:53 +0700

    client: Conditionally making queries

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -2,18 +2,22 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useLocation } from "react-router";
 import type { FieldValues } from "react-hook-form";
+import { useAccount } from "./useAccount";
 
 export const useActivities = (id?: string) => {
   const queryClient = useQueryClient();
+  const { currentUser } = useAccount();
   const location = useLocation();
 
-  const { data: activities, isPending } = useQuery({
+  //Doc of isPending: Will be pending if there's no cached data and no query attempt was finished yet.
+  //That means isPending will be true although don't go to api to fetch data yet
+  const { data: activities, isLoading } = useQuery({
     queryKey: ["activities"],
     queryFn: async () => {
       const response = await agent.get<Activity[]>("/activities");
       return response.data;
     },
-    enabled: !id && location.pathname === "/activities",
+    enabled: !id && location.pathname === "/activities" && !!currentUser,
   });
 
   const { data: activity, isLoading: isLoadingActivity } = useQuery({
@@ -22,7 +26,7 @@ export const useActivities = (id?: string) => {
       const respone = await agent.get<Activity>(`activities/${id}`);
       return respone.data;
     },
-    enabled: !!id,
+    enabled: !!id && !!currentUser,
   });
 
   const updateActivity = useMutation({
@@ -61,7 +65,7 @@ export const useActivities = (id?: string) => {
 
   return {
     activities,
-    isPending,
+    isLoading,
     updateActivity,
     createActivity,
     deleteActivity,
